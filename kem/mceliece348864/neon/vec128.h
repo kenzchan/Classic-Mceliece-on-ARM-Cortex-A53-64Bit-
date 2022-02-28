fed89dfe092a5cc5155b80b643087cf1a2e1a514
kenzchan <59307794+kenzchan@users.noreply.github.com>
2022-03-01 07:53:05 +0800

    update vec128_mul stp/ldp

--- kem/mceliece348864/neon/vec128.h
+++ kem/mceliece348864/neon/vec128.h
@@ -199,31 +199,33 @@ static inline vec128 vec128_or_reduce(vec128 * a)
 	return ret;
 }
 
-static inline void vec128_mul(uint64x2_t *h, uint64x2_t *f, const uint64x2_t *g)
+static void vec128_mul(uint64x2_t *h, uint64x2_t *f, const uint64x2_t *g)
 {
 	asm volatile (
-		// load f[0] - f[11]
-		"ldp q0, q1, [%[x1]] \n"					//f[0], f[1]
-		"ldp q2, q3, [%[x1], #16*2] \n"		//f[2], f[3]
-		"ldp q4, q5, [%[x1], #16*4] \n"	//f[4], f[5]
-		"ldp q6, q7, [%[x1], #16*6] \n"	//f[6], f[7]
-		"ldp q8, q9, [%[x1], #16*8] \n"	//f[8], f[9]
-		"ldp q10, q11, [%[x1], #16*10] \n"//f[10],f[11]
-
 		// load g[10] g[11]
 		"ldp q12, q13, [%[x2], #16*10] \n"
-
+		"ldp q10, q11, [%[x1], #16*10] \n"//f[10],f[11]
 		//compute buf with g[11]
 		"and v31.16b, v13.16b, v11.16b \n"//buf[22] = f11g11
 		"and v30.16b, v13.16b, v10.16b \n"//buf[21] = f10g11
+
+		"ldp q8, q9, [%[x1], #16*8] \n"	//f[8], f[9]
 		"and v29.16b, v13.16b, v9.16b \n"//buf[20] = f9g11
 		"and v28.16b, v13.16b, v8.16b \n"//buf[19] = f8g11
+
+		"ldp q6, q7, [%[x1], #16*6] \n"	//f[6], f[7]
 		"and v27.16b, v13.16b, v7.16b \n"//buf[18] = f7g11
 		"and v26.16b, v13.16b, v6.16b \n"//buf[17] = f6g11
+
+		"ldp q4, q5, [%[x1], #16*4] \n"	//f[4], f[5]
 		"and v25.16b, v13.16b, v5.16b \n"//buf[16] = f5g11
 		"and v24.16b, v13.16b, v4.16b \n"//buf[15] = f4g11
+
+		"ldp q2, q3, [%[x1], #16*2] \n"		//f[2], f[3]
 		"and v23.16b, v13.16b, v3.16b \n"//buf[14] = f3g11
 		"and v22.16b, v13.16b, v2.16b \n"//buf[13] = f2g11
+
+		"ldp q0, q1, [%[x1]] \n"					//f[0], f[1]
 		"and v21.16b, v13.16b, v1.16b \n"//buf[12] = f1g11
 		"and v20.16b, v13.16b, v0.16b \n"//buf[11] = f0g11
 
@@ -393,7 +395,7 @@ static inline void vec128_mul(uint64x2_t *h, uint64x2_t *f, const uint64x2_t *g)
 		"eor v30.16b, v30.16b, v16.16b \n"//buf[9] = f0g9^f1g8^f2g7^f3g6^(buf[21]^buf[18])
 		"eor v29.16b, v29.16b, v17.16b \n"//buf[8] = f0g8^f1g7^f2g6^(buf[20])
 		"eor v28.16b, v28.16b, v18.16b \n"//buf[7] = f0g7^f1g6^(buf[19])
-		"eor v27.16b, v27.16b, v19.16b \n"//buf[6] = f0g6^f0g6^(buf[18])
+		"eor v27.16b, v27.16b, v19.16b \n"//buf[6] = f0g6^(buf[18])
 
 		
 		//load g[4] g[5]
@@ -578,10 +580,15 @@ static inline void vec128_mul(uint64x2_t *h, uint64x2_t *f, const uint64x2_t *g)
 
 		"eor v20.16b, v20.16b, v14.16b \n"//buf[11] = f0g11^f1g10^f3g8^f4g7^f5g6^f6g5^f7g4^f8g3^f9g2^f10g1^f11g0^(buf[20])
 		"eor v31.16b, v31.16b, v15.16b \n"//buf[10] = f0g10^f2g8^f3g7^f4g6^f5g5^f6g4^f7g3^f8g2^f9g1^f10g0^(buf[22]^buf[19])
+		"stp q31, q20, [%[x0], #16*10] \n" //store 10,11
+
 		"eor v30.16b, v30.16b, v16.16b \n"//buf[9] = f0g9^f1g8^f2g7^f3g6^f4g5^f5g4^f6g3^f7g2^f8g1^f9g0^(buf[21]^buf[18])
 		"eor v29.16b, v29.16b, v17.16b \n"//buf[8] = f0g8^f1g7^f2g6^f3g5^f4g4^f5g3^f6g2^f7g1^f8g0^(buf[20]^buf[17])
+		"stp q29, q30, [%[x0], #16*8] \n" //store 8,9
+
 		"eor v28.16b, v28.16b, v18.16b \n"//buf[7] = f0g7^f1g6^f2g5^f3g4^f4g3^f5g2^f6g1^f7g0^(buf[19]^buf[16])
 		"eor v27.16b, v27.16b, v19.16b \n"//buf[6] = f0g6^f0g6^f1g5^f2g4^f3g3^f4g2^f5g1^f6g0^(buf[18])
+		"stp q27, q28, [%[x0], #16*6] \n" //store 6,7
 
 		//"str q21, [%[x0], #16*12] \n"
 
@@ -595,16 +602,14 @@ static inline void vec128_mul(uint64x2_t *h, uint64x2_t *f, const uint64x2_t *g)
 
 		"eor v26.16b, v26.16b, v14.16b \n"//buf[5] = f0g5^f1g4^f2g3^f3g2^f4g1^f5g0^(buf[17])
 		"eor v25.16b, v25.16b, v15.16b \n"//buf[4] = f0g4^f1g3^f2g2^f3g1^f4g0^(buf[16])
+		"stp q25, q26, [%[x0], #16*4] \n" //store 4,5
+
 		"eor v24.16b, v24.16b, v16.16b \n"//buf[3] = f0g3^f1g2^f2g1^f3g0^(buf[15])
 		"eor v23.16b, v23.16b, v17.16b \n"//buf[2] = f0g2^f1g1^f2g0^(buf[14])
+		"stp q23, q24, [%[x0], #16*2] \n" //store 2,3
+
 		"eor v22.16b, v22.16b, v18.16b \n"//buf[1] = f0g1^f1g0^(buf[13])
 		"eor v21.16b, v21.16b, v19.16b \n"//buf[0] = f0g0^(buf[12])
-
-		"stp q31, q20, [%[x0], #16*10] \n" //store 10,11
-		"stp q29, q30, [%[x0], #16*8] \n" //store 8,9
-		"stp q27, q28, [%[x0], #16*6] \n" //store 6,7
-		"stp q25, q26, [%[x0], #16*4] \n" //store 4,5	
-		"stp q23, q24, [%[x0], #16*2] \n" //store 2,3
 		"stp q21, q22, [%[x0]] \n" //store 0,1
 
     : [x0] "+r" (h)
